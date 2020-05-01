4b3ea1cbaf87a14dd954b9b58d127facf12e3aa8
Tony <webmaster@iotcrazy.com>
2020-05-01 11:14:04 +0100

    Removed unused variables

--- simple-joystick.ino
+++ simple-joystick.ino
@@ -2,6 +2,7 @@
 // Simple joystick tutorial with smoothing
 
 // Which pins the joystick is connected to
+// These values are for the ESP 32
 int JoyStick_X = 36; // x
 int JoyStick_Y = 39; // y
 int JoyStick_Z = 35; // switch - remember the pullup resistor!
@@ -9,11 +10,6 @@ int JoyStick_Z = 35; // switch - remember the pullup resistor!
 int prevx, prevy, prevbtn = 0; // previous states or each joystick input, default to zero
 int minChange = 20; // minimum amount of change that must be seen to determine that the stick has moved
 int numSamples = 20; // how many samples to take when reading the joystick
-float stickMax = 4290; // the maximum expected value from joystick axes 
-float stickMin = 0; // the minimum expected value from joystick axes
-float stickMidX = 1913; // the middle value of the X axis - found my running the program and looking at the output
-float stickMidY = 1959;  // the middle value of the Y axis - found my running the program and looking at the output
-
 
 void setup() {
   Serial.begin(115200);
